28d728e3c0434524b0f759f088c7d672902f7f53
g6vc <christian.bartram@bcbsfl.com>
2017-07-18 10:26:41 -0400

    Completed Amazon Alexa Skill

--- apps/RideTimes/APIDataHelper.js
+++ apps/RideTimes/APIDataHelper.js
@@ -5,58 +5,32 @@ const rp = require('request-promise');
 const BASE_URL = 'http://wait-smart.herokuapp.com/api/v1';
 
 module.exports = {
-        /**
-         * Finds the Ride Wait time given the rides name
-         * @param rideName
-         * @param callback
-         */
-        getRideWaitTime(rideName, callback) {
-            this.getIDforName(rideName, (data) => {
-                //Data is the ID being returned from the ID -> name callback
-                let options = {
-                    method: 'GET',
-                    uri: `${BASE_URL}/fetch/${data}`,
-                    resolveWithFullResponse: true,
-                    json: true
-                };
-
-                rp(options).then((res) => {
-                    callback(res.body.id, res.body.name, res.body.wait_time);
-                });
-            });
-        },
-
     /**
      * Finds the Average Wait time for a park given the parks name
      * @param park
-     * @param callback
      */
-    getAverageParkWaitTime(park, callback) {
+    getAverageParkWaitTime(park) {
         let options = {
             method: 'GET',
             uri: `${BASE_URL}/park/${park}`,
             resolveWithFullResponse: true,
             json: true
         };
-        rp(options).then((res) => {
-            callback(res.body.park, res.body.wait);
-        });
+
+       return rp(options);
     },
 
     /**
      * Maps a Ride Name to its ID
      * @param name
-     * @param callback
      */
-    getIDforName(name, callback) {
+    getRideWaitTime(name) {
         let options = {
             method: 'GET',
             uri: `${BASE_URL}/map/name/${name}`,
             resolveWithFullResponse: true,
             json: true
         };
-        rp(options).then((res) => {
-            callback(res.body[0].id);
-        });
+        return rp(options);
     }
 };
